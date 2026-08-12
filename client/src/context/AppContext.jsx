e7dee9e54141334ceaa1c6feee39af2394863117
ASADNAWAZ10 <asadnawazasadnawaz211@gmail.com>
2026-08-11 22:03:27 -0600

    added vercel link

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import axios from 'axios'
 import toast from "react-hot-toast";
 
-axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL;
+axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL || "https://quick-gpt-server-alpha-one.vercel.app";
 
 
 const AppContext = createContext();
