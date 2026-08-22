6ed67c4b477a5f99933c94dc911f85cde9789f6f
ASADNAWAZ10 <asadnawazasadnawaz211@gmail.com>
2026-08-22 10:24:06 -0600

    change

--- server/controller/messageController.js
+++ server/controller/messageController.js
@@ -62,7 +62,7 @@ export const imageGeneratorController = async (req, res) => {
         // const encodedPrompt = encodeURIComponent(prompt)
 
         const result = await openai.images.generate({
-            model: "gemini-2.5-flash",
+            model: "imagen-4",
             prompt: prompt,
             size: "1024 * 1024",
             quality: "standard",
